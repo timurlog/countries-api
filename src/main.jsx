e8ecc89106fc04ba02655c9566bccfb85dff1436
timurlog <timur.logie@gmail.com>
2024-03-07 14:11:27 +0100

    modified main.jsx corect syntax

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/countries-api/",
-    element: <App />,
-  },
-]);
+export default function Main() {
+  const router = createBrowserRouter([
+    {
+      path: "/countries-api/",
+      element: <App />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Main />
   </React.StrictMode>
 );
