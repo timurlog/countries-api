08f6f5b17487be44ce38fb6c4e536c24d15b37e9
timurlog <timur.logie@gmail.com>
2024-03-08 17:26:12 +0100

    modified Countrie.jsx added useParams and select the correct countrie with useEffect

--- src/pages/Countrie.jsx
+++ src/pages/Countrie.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { DataContext } from "../main";
 
 export default function Countrie() {
+  const { data, setData } = useContext(DataContext);
+
+  const { countrieId } = useParams();
+  const [countrie, setCountrie] = useState([]);
+
+  useEffect(() => {
+    setCountrie(
+      data.filter((item) =>
+        item.name.common.toLowerCase().includes(countrieId.toLowerCase())
+      )
+    );
+  }, [countrieId, data]);
+
   return (
     <div>
       <div></div>
