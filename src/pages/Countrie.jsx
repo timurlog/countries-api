739d0e83b0e43939caebfdee58bf35f60fef84bb
timurlog <timur.logie@gmail.com>
2024-03-10 14:59:49 +0100

    modified Countrie.jsx added back btn, Navbar, Footer

--- src/pages/Countrie.jsx
+++ src/pages/Countrie.jsx
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { DataContext } from "../main";
+import { ThemeContext } from "../main";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Copyright from "../components/Copyright/Copyright";
 
 export default function Countrie() {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
   const { data, setData } = useContext(DataContext);
 
   const { countrieId } = useParams();
@@ -18,8 +24,30 @@ export default function Countrie() {
   }, [countrieId, data]);
 
   return (
-    <div>
-      <div></div>
+    <div
+      className={`${
+        darkMode ? "bg-[#2F2F2C]" : "bg-white"
+      } h-screen flex flex-col justify-between`}
+    >
+      <Navbar />
+      <div className="px-5 md:px-10">
+        <Link to={"/countries-api/countries/"}>
+          <div
+            className={`${
+              darkMode
+                ? "bg-[#1D1D1B] text-white hover:bg-[#181817]"
+                : "bg-[#cecece] text-[#2F2F2C] hover:bg-[#acacaa]"
+            } rounded-lg flex gap-3 justify-center items-center py-3 w-24 transition-colors`}
+          >
+            <i className="fa-solid fa-arrow-left"></i>Back
+          </div>
+        </Link>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <Copyright />
     </div>
   );
 }
